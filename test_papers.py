2ddb3353e0f22c1b27b6659034b4475bf90ae38d
structur <childeatsdog@gmail.com>
2014-11-10 18:53:05 -0500

    added tests and test.jsons

--- test_papers.py
+++ test_papers.py
@@ -22,14 +22,14 @@ def test_basic():
     assert decide("test_req_keys.json", "watchlist.json", "countries.json") == \
           ["Reject", "Reject", "Reject", "Reject", "Reject", "Reject", "Reject", \
            "Reject", "Reject", "Reject", "Reject", "Reject", "Reject", "Reject"]
-    #assert decide("test_transit_visa", "watchlist.json", "countries.json") == ['Reject']
-    #assert decide("test_visit_visa", "watchlist.json", "countries.json") == ['Reject']
-    #assert decide("test_passport_format", "watchlist.json", "countries.json") == ['Reject']
+    assert decide("test_transit_visa", "watchlist.json", "countries.json") == ['Reject', 'Reject']
+    assert decide("test_visit_visa", "watchlist.json", "countries.json") == ['Reject', 'Reject']
+    assert decide("test_passport_format.json", "watchlist.json", "countries.json") == ['Accept', 'Reject']
 
 def test_files():
     with pytest.raises(FileNotFoundError):
         decide("test_returning_citizen.json", "", "countries.json")
 
-#def test_date_format():
-    #assert decide("test_date_format.json",  "watchlist.json", "countries.json")
+def test_date_format():
+    assert decide("test_date_format.json",  "watchlist.json", "countries.json")
 # add functions for other tests
